ad92735f8d3132ceb2a947f62b1cabe9cb533fda
wanessamarcondes <wanessamarcondes@gmail.com>
2018-12-05 16:54:04 -0200

    in progress

---
+++ script.js
@@ -0,0 +1,25 @@
+const ep1 = document.getElementById("episode1")
+const ep2 = document.getElementById("episode2")
+const ep3 = document.getElementById("episode3")
+
+ep1.addEventListener("click", function(){
+    // event.preventDefault();
+    // ep1.classList.remove("hide")
+    // ep2.classList.add("hide")
+    // ep3.classList.add("hide")
+    console.log("eita")
+})
+
+ep2.addEventListener("click", function(){
+    // ep1.classList.add("hide")
+    // ep2.classList.remove("hide")
+    // ep3.classList.add("hide")
+    console.log("eita2")
+})
+
+ep3.addEventListener("click", function(){
+    // ep1.classList.add("hide")
+    // ep2.classList.add("hide")
+    // ep3.classList.remove("hide")
+    console.log("eita3")
+})
